38d657d2ac7ec4b4ee22b2ba3df4ded371f08d76
Paolo Bergamelli <cobertoo.dev@gmail.com>
2024-03-20 20:43:43 +0100

    header

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+  <link rel="icon" type="image/x-icon" href="/wp-content/themes/carrefour-viajes-agencias/dist/img/favicon.ico">
 	<?php wp_head(); ?>
 </head>
 
@@ -26,9 +26,9 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'carrefour-viajes-agencias' ); ?></a>
 
 <header
-  class="flex static flex-wrap justify-between items-center py-2 px-4 m-0 text-sm leading-3 align-baseline border-t-0 border-b border-solid xl:px-12 xl:pt-8 xl:pb-1 lg:relative lg:w-full lg:px-5 lg:pt-8 lg:pb-1 border-x-0 border-neutral-200 text-stone-500"
+  class="flex static flex-wrap justify-between items-center py-2 px-4 m-0 text-sm leading-3 align-baseline border-t-0 border-b border-solid xl:px-12 xl:pt-8 lg:relative lg:w-full lg:px-5 lg:pt-8 border-x-0 border-neutral-200 text-stone-500 mb-5 pb-5"
 >
-  <div class="p-0 m-0 align-baseline border-0 lg:-order-1">
+  <div class="logo-container p-0 m-0 align-baseline border-0 lg:-order-1">
     <div class="p-0 m-0 align-baseline border-0 lg:inline-block">
       <a
         href="https://www.viajes.carrefour.es/"
@@ -38,55 +38,37 @@
           src="https://www.viajes.carrefour.es/statics/assets/images/base/crf_viajes_logo_h_colour_rgb-01.png"
           alt="Logo desktop"
       /></a>
+      <h1
+        class="px-0 pt-1 pb-0 m-0 w-full font-bold text-center align-baseline border-0 lg:pl-3 lg:text-left text-xs text-blue-dark"
+      >
+        Ofertas en viajes, hoteles, vuelos, cruceros
+      </h1>
     </div>
   </div>
 
   <div
-    class="op-4 m-0 w-full h-2 align-baseline bg-white border-0 lg:relative lg:top-0 lg:left-0 lg:-order-1 lg:h-auto lg:w-3/4 lg:bg-transparent lg:p-0"
+    class="right-header"
   >
-    <div
-      class=" p-0 m-0 text-xs text-white uppercase align-baseline border-0 md:block"
-    >
-      <ul
-        class="p-0 m-0 uppercase align-baseline border-0 lg:flex lg:items-center lg:justify-end"
-      >
-        <li
-          class="px-0 pt-0 pb-2 mx-0 mt-0 mb-2 text-left uppercase align-baseline border-t-0 border-b border-solid lg:mb-0 lg:border-b-0 lg:pb-0 border-x-0 border-zinc-100"
-        >
-          <a
-            href="tel:919100057"
-            class="p-0 m-0 uppercase align-baseline bg-transparent border-0 cursor-pointer lg:uppercase lg:text-sky-800 hover,lg:text-sky-500 content-none hover:text-sky-800 hover:opacity-80"
-            ><em
-              class="inline-block p-0 my-0 mr-1 ml-0 not-italic leading-none normal-case align-baseline border-0 text-sky-800"
-            >
-            </em
-            >919 100 057</a
-          >
-        </li>
-        <li
-          class="px-0 pt-0 pb-2 mx-0 mt-0 mb-2 text-left uppercase align-baseline border-t-0 border-b border-solid lg:mb-0 lg:border-b-0 lg:pb-0 border-x-0 border-zinc-100"
-        >
-          <a
-            href="https://www.viajes.carrefour.es/agencias-viajes/"
-            class="p-0 m-0 uppercase align-baseline bg-transparent border-0 cursor-pointer lg:uppercase lg:text-sky-800 hover,lg:text-sky-500 content-none hover:text-sky-800 hover:opacity-80"
-            ><em
-              class="inline-block p-0 my-0 mr-1 ml-0 not-italic leading-none normal-case align-baseline border-0 text-sky-800"
-            >
-            </em
-            >Agencias</a
-          >
-        </li>
-      </ul>
+    <div class="preheader">
+    <ul class="header-elements">
+    <li><a href="tel:919100057"><em class="icon-phone"> </em>919 100 057</a></li>
+    <li><a href="https://www.viajes.carrefour.es/agencias-viajes/"><em class="icon-ubication"> </em>Agencias</a></li>
+    </ul>
     </div>
-    <nav class="block p-0 m-0 align-baseline border-0" role="navigation">
-      nav here
+    <nav class="main-menu p-0 m-0 align-baseline border-0" role="navigation">
+      <ul class="header-elements">
+      <li><a href="https://www.viajes.carrefour.es/circuitos" title="Circuitos">Circuitos</a></li>
+      <li><a href="https://www.viajes.carrefour.es/viajar/egipto" title="Egipto">Egipto 75%</a></li>
+      <li><a href="https://www.viajes.carrefour.es/cruceros" title="Cruceros">Cruceros</a></li>
+      <li><a href="https://www.viajes.carrefour.es/viajar/caribe" title="Viajes al Caribe">Caribe</a></li>
+      <li><a href="https://www.viajes.carrefour.es/disney" title="Ofertas viajes Disneyland Paris">Disneyland Paris</a></li>
+      <li><a href="https://www.viajes.carrefour.es/portaventura" title="Ofertas Portaventura">Portaventura</a></li>
+      <li><a href="https://www.viajes.carrefour.es/hoteles" title="Hoteles baratos">Hoteles</a></li>
+      <li><a href="https://www.viajes.carrefour.es/vuelo-hotel" title="Vuelo más hotel barato">Vuelo + hotel</a></li>
+      <li><a href="https://www.viajes.carrefour.es/vuelos" title="Vuelos baratos">Vuelos</a></li>
+      </ul>
     </nav>
     
   </div>
-  <h1
-    class="px-0 pt-1 pb-0 m-0 w-full font-bold text-center align-baseline border-0 lg:pl-3 lg:text-left lg:text-xs text-blue-dark"
-  >
-    Ofertas en viajes, hoteles, vuelos, cruceros
-  </h1>
 </header>
 
