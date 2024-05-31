0c41413ff33ef1b7f121510f916b8953cb57dfcb
Paolo Bergamelli <bergamelli.p@gmail.com>
2024-05-31 22:28:47 +0200

    gelocate button position

--- template-parts/buscador.php
+++ template-parts/buscador.php
@@ -1,4 +1,4 @@
-<div class="mb-10 max-w-2xl">
+<div class="mb-10">
 	<div class="md:flex">
 		<form action="" class="buscador mb-5 md:mb-0">
 			<select name="search" id="search-agencias" class="">
@@ -7,9 +7,9 @@
 			<button class="rounded-tr rounded-br bg-blue-dark text-white p-2 inline-block border border-blue-dark ml-auto h-full" style="width:42px"><span class="icon-search"></span></button>
 		</form>
 		<?php if(is_front_page()){ ?>
-			<a href="javascript:void(0);" onclick="loadGoogleMap();" class="mr-auto bg-blue-dark text-white text-bold px-5 py-2 flex items-center whitespace-nowrap justify-center rounded"><span class="vc-icon-map text-2xl"></span>Buscar agencias cerca</a>
+			<a href="javascript:void(0);" onclick="loadGoogleMap();" class="ml-auto bg-blue-dark text-white text-bold px-5 py-2 flex items-center whitespace-nowrap justify-center rounded"><span class="vc-icon-map text-2xl"></span>Buscar agencias cerca</a>
 		<?php } else { ?>
-			<a href="<?php echo bloginfo('url'); ?>" class="mr-auto bg-blue-dark text-white text-bold px-5 py-2 flex items-center whitespace-nowrap justify-center rounded"><span class="vc-icon-map text-2xl"></span>Buscar agencias cerca</a>
+			<a href="<?php echo bloginfo('url'); ?>" class="ml-auto bg-blue-dark text-white text-bold px-5 py-2 flex items-center whitespace-nowrap justify-center rounded"><span class="vc-icon-map text-2xl"></span>Buscar agencias cerca</a>
 		<?php } ?>
 	</div>	
 </div>	
